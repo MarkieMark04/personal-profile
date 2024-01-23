0d447079d62c5fe290531ba3f5f9431ddfe99621
markiemark004 <markie.markyum@gmail.com>
2024-01-23 14:47:57 +1300

    Working 1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* ============================== Typing Animation ============================== */
+const typed = new Typed(".typing", {
+  strings: ["Web Designer", "Web Developer", "Graphic Designer", "YouTuber"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
+
+/* ============================== Menu Display ============================== */
---
+++ js/style-switcher.js
@@ -0,0 +1,56 @@
+/* ============================== Toggler Style Switcher ============================== */
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+
+styleSwitcherToggle.addEventListener("click", () => {
+  document.querySelector(".style-switcher").classList.toggle("open");
+});
+
+// Hide Style - Switcher On Scroll
+window.addEventListener("scroll", () => {
+  if (document.querySelector(".style-switcher").classList.contains("open")) {
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+});
+
+/* ============================== Theme Colours ============================== */
+const alternateStyles = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(colour) {
+  alternateStyles.forEach((style) => {
+    if (colour === style.getAttribute("title")) {
+      style.removeAttribute("disabled");
+    } else {
+      style.setAttribute("disabled", "true");
+    }
+  });
+}
+
+setActiveStyle("colour-5");
+
+/* ============================== Theme Light and Dark Mode ============================== */
+const daynight = document.querySelector(".day-night");
+const logo = document.querySelector("#logo");
+
+let isFirstImage = true;
+
+daynight.addEventListener("click", () => {
+  if (isFirstImage) {
+    logo.src = "images/Markie No BG WHITE TEXT.png";
+  } else {
+    logo.src = "images/Markie No BG.png";
+  }
+
+  isFirstImage = !isFirstImage;
+
+  daynight.querySelector("i").classList.toggle("fa-sun");
+  daynight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+});
+
+window.addEventListener("load", () => {
+  if (document.body.classList.contains("dark")) {
+    daynight.querySelector("i").classList.add("fa-sun");
+  } else {
+    daynight.querySelector("i").classList.add("fa-moon");
+  }
+});
